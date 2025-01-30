c74b155dda6f2ab59893b7a53db4fcf63a46f36d
Ab_jimie <102118435+Bem20-james@users.noreply.github.com>
2025-01-30 13:13:19 +0100

    base_url path updated

--- src/config/path.js
+++ src/config/path.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://iwc-airdrop-backend-1.onrender.com/V1"
+export const BASE_URL = "https://iwc-airdrop-backend-mkaq.onrender.com/V1"
 export const API_BASE_URL = "iwc_airdrop_db"
